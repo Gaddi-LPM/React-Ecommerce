b3f35d29d8163b749d9eb7c9de26fbeeb99f9b0b
Matias Gaddi <gaddi.matias@gmail.com>
2022-08-30 10:02:38 -0300

    NavBar

---
+++ src/componets/NavBar/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./style.css";
+
+const NavBar = () => {
+  return (
+    <div>
+        <div className='NavBar'>
+          <h1>E-JACARANDA</h1>
+         <a href="#inicio">Inicio</a>
+         <a href="#productos">Productos</a>
+         <a href="#servicios">Servicios</a>
+         <a href="#nosotros">Nosotros</a>
+         <a href="#contacto">Contacto</a>
+      </div>
+      
+    </div>
+  )
+}
+
+export default NavBar
