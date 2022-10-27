52a0f80ced9f66d47273b5fbb6c90282da2b00d3
Matias Gaddi <gaddi.matias@gmail.com>
2022-10-27 00:52:56 -0300

    Mod Detalle

--- src/Containers/CartContainer/index.jsx
+++ src/Containers/CartContainer/index.jsx
@@ -16,19 +16,17 @@ const CartContainer = () => {
   
   const {cart, cleanCart, total} = useContext(Shop);
   const [loading , setLoading] = useState(false);
-  const [openform , setOpenForm] = useState(false)
-  const navigate = useNavigate()
+  const [openform , setOpenForm] = useState(false);
   const [data , setData]= useState({
-
+    
         nombre:"",
         email:"",
         telefono:""
-  })
-  
-
-  
+      })
+  const navigate = useNavigate()
+      
   const carro = cart.find(item => item.title !== "");
-  
+ 
   const back = ()=>{
     navigate('/');
   }
@@ -57,12 +55,18 @@ const CartContainer = () => {
       Swal.fire({
         title: 'GRACIAS! :)',
         text: `Se genero orden con ID: ${docRef.id}`,
-        html: `<p>${orden.buyer.nombre}<p/>
-               <p>${orden.buyer.telefono}</p>
-               <p>${orden.buyer.email}</p>`,      
+        html: `<p>Se genero orden con ID: <strong>${docRef.id}</strong> </p>
+               <p>Nombre: <strong>${orden.buyer.nombre}</strong> </p>
+               <p>Telefono: <strong>${orden.buyer.telefono}</strong> </p>
+               <p>Email: <strong>${orden.buyer.email}</strong> </p>
+               <ul>
+                  <li>Items: <strong>${cart.map(item => item.title)}</strong> </li>
+                </ul>
+               <p>Total $: <strong>${orden.total}</strong> </p>
+               <p>Fecha: <strong>${orden.createdAt}</strong> </p>`,      
         icon: 'success',
         imageWidth: 300,
-        imageHeight: 200,
+        imageHeight: 300,
         imageAlt: 'Custom image',
       })
       
